18008d22e183d6d3ec04f36c5201ef2ecf3fc9df
iliyastb <skiliyas300@gmail.com>
2024-05-07 18:46:21 +0530

    Update

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,17 +2,25 @@ def call() {
   pipeline {
     agent any
 
+    parameters {
+      string(name: 'ENV', defaultValue: '', description: 'which environment?')
+    }
+
+    options {
+      ansicolor('xterm')
+    }
+
     stages {
 
       stage('Init') {
         steps {
-          sh 'terraform init -backend-config=env-dev/state.tfvars'
+          sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
         }
       }
 
       stage('Apply') {
         steps {
-//          sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+//          sh 'terraform apply -auto-approve -var-file=env-${ENV}/main.tfvars'
           sh 'echo'
         }
       }
