520026347427f6cee63545bee1ef79af719a667a
iliyastb <skiliyas300@gmail.com>
2024-05-08 16:27:42 +0530

    Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,6 +15,6 @@ def testcases() {
   sh 'echo OK'
 }
 
-def codequlaity() {
+def codequality() {
   sh 'sonar-scanner -Dsonar.host.url=http://172.31.18.171:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component}'
 }
