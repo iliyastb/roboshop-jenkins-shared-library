40fe8788ad3947b8bf3e21e46d49c1e0ad8ac152
iliyastb <skiliyas300@gmail.com>
2024-05-08 15:04:19 +0530

    Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,17 +1,17 @@
 def compile() {
-  if(app_lang == nodejs) {
+  if(app_lang == "nodejs") {
     sh 'npm install'
   }
-  if(app_lang == maven) {
+  if(app_lang == "maven") {
     sh 'mvn package'
   }
 }
 
 def testcases() {
-  if(app_lang == nodejs) {
+  if(app_lang == "nodejs") {
     sh 'echo test'
   }
-  if(app_lang == maven) {
+  if(app_lang == "maven") {
     sh 'echo test'
   }
 }
