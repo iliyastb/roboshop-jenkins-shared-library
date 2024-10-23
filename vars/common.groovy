8b89afa47306922d23a874a8d756553ac684e712
iliyastb <skiliyas300@gmail.com>
2024-10-23 18:45:32 +0530

    Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -17,6 +17,6 @@ def testcases() {
 
 def codequality() {
   withAWSParameterStore(credentialsId: 'param', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
-    sh 'sonar-scanner -Dsonar.host.url=http://172.31.18.171:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+    sh 'sonar-scanner -Dsonar.host.url=http://172.31.26.160:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
   }
 }
