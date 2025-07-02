1f0f949f975311a29164f14d194d7cec35c382f8
iliyastb <skiliyas300@gmail.com>
2025-07-02 22:08:48 +0530

    Update

---
+++ vars/ci-old.groovy
@@ -0,0 +1,27 @@
+def call() {
+  if (!env.sonar_extra_opts) {
+    env.sonar_extra_opts = ""
+  }
+  node('worstation') {
+
+    try {
+      stage('Compile/Build') {
+        sh 'env'
+        common.compile()
+      }
+
+      stage('Compile/Build') {
+        common.testcases()
+      }
+
+      stage('Compile/Build') {
+        common.codequality()
+      }
+
+    } catch (e) {
+      echo "ok"
+//      mail body: "<h3>${component} - Pipeline failed \n ${BUILD_URL}</h3>", from: 'skiliyas300@gmail.com', mimeType: 'text/html', subject: "${component} - Pipeline failed", to: 'skiliyas300@gmail.com'
+    }
+
+  }
+}
