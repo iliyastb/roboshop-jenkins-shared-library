13595e85678931d6c2b8a4d559af095b6c77edbc
iliyastb <skiliyas300@gmail.com>
2025-07-16 17:42:21 +0530

    Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -24,11 +24,10 @@ def codequality() {
 
 def prepareArtifacts() {
   sh 'echo ${TAG_NAME} >VERSION'
-  if (app_lang == "nodejs" || app_lang == "angular") {
-    sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
-  }
   if (app_lang == "maven") {
     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+  } else {
+    sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
   }
 }
 
