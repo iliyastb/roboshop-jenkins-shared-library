5de6a11d8b5f4905a79dcb339f9121488e3b8292
iliyastb <skiliyas300@gmail.com>
2025-07-16 21:46:19 +0530

    Update

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,37 +1,54 @@
 def call() {
-  pipeline {
-    agent any
 
-//    parameters {
-//      string(name: 'ENV', defaultValue: '', description: 'which environment?')
-//    }
+  env.ENV = "prod"
 
-    options {
-      ansiColor('xterm')
+  node('workstation') {
+
+    stage('Init') {
+      sh 'terraform init -backend-config=env-${env.ENV}/state.tfvars'
     }
 
-    stages {
-
-      stage('Init') {
-        steps {
-          sh 'terraform init -backend-config=env-prod/state.tfvars'
-        }
-      }
-
-      stage('Apply') {
-        steps {
-          sh 'terraform apply -auto-approve -var-file=env-prod/main.tfvars'
-//          sh 'echo'
-        }
-      }
-
-    }
-
-    post {
-      always {
-        cleasWs()
-      }
+    stage('Apply') {
+      sh 'terraform apply -auto-approve -var-file=env-${env.ENV}/main.tfvars'
     }
 
   }
 }
+
+//def call() {
+//  pipeline {
+//    agent any
+//
+//    parameters {
+//      string(name: 'ENV', defaultValue: '', description: 'which environment?')
+//    }
+//
+//    options {
+//      ansiColor('xterm')
+//    }
+//
+//    stages {
+//
+//      stage('Init') {
+//        steps {
+//          sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
+//        }
+//      }
+//
+//      stage('Apply') {
+//        steps {
+//          sh 'terraform apply -auto-approve -var-file=env-${ENV}/main.tfvars'
+////          sh 'echo'
+//        }
+//      }
+//
+//    }
+//
+//    post {
+//      always {
+//        cleasWs()
+//      }
+//    }
+//
+//  }
+//}
