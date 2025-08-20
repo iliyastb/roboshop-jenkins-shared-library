d43560a11d6b5c69ac15b33f3c768408f1400c5b
iliyastb <skiliyas300@gmail.com>
2025-08-20 17:17:16 +0530

    Update

---
+++ vars/helm.groovy
@@ -0,0 +1,42 @@
+def call() {
+  pipeline {
+    agent any
+
+    options {
+      ansiColor('xterm')
+    }
+
+    parameters {
+      string(name: 'app_version', defaultValue: '', description: 'App Version')
+      string(name: 'component', defaultValue: '', description: 'Component')
+      string(name: 'environment', defaultValue: '', description: 'Environment')
+    }
+
+    stages {
+
+      stage('Clone Application') {
+        steps {
+          dir('APP') {
+            git branch: 'main', url: "https://github.com/iliyastb/${component}"
+          }
+        }
+      }
+
+      stage('Deploy Helm Chart') {
+        steps {
+          script {
+            sh 'helm upgrade -i ${component} HELM/ -f APP/helm/${environment}.yaml --set appversion=${app_version}'
+          }
+        }
+      }
+
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+
+  }
+}
