84e67c15816cf8e487dbe62bdfe682b10a194e4b
iliyastb <skiliyas300@gmail.com>
2025-08-20 21:16:26 +0530

    Update

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -19,13 +19,16 @@ def call() {
           dir('APP') {
             git branch: 'main', url: "https://github.com/iliyastb/${component}"
           }
+          dir('HELM') {
+            git branch: 'main', url: "https://github.com/iliyastb/roboshop-helm-chart"
+          }
         }
       }
 
       stage('Deploy Helm Chart') {
         steps {
           script {
-            sh 'helm upgrade -i ${component} . -f APP/helm/${environment}.yaml --set appversion=${app_version}'
+            sh 'helm upgrade -i ${component} HELM/ -f APP/helm/${environment}.yaml --set appversion=${app_version}'
           }
         }
       }
